7aa12a2dd1bb6d591b2e64f7747ab5cf11efce1c
Juliette Tisseyre <juliette.tisseyre@deepomatic.com>
2022-01-03 17:36:31 +0100

    Add new packages to install

--- configuration_files/install_basic_packages.sh
+++ configuration_files/install_basic_packages.sh
@@ -4,10 +4,24 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get autoremove
 
-sudo apt-get install emacs colordiff cloc sloccount gitk tree vlc htop irssi gparted gimp
+sudo apt-get install emacs colordiff cloc sloccount \
+     gitk tree vlc htop irssi gparted gimp signal-desktop \
+     redis-tools jq graphviz gimp gh
 snap install skype
 snap install spotify
+snap vlc
+snap slack
 
 # Manual install of:
 #  - VirtualBox
 #  - Jetbrains ToolBox
+# libreoffice
+# docker
+# docker compose
+# discord
+
+# For bash configuration
+# https://github.com/jimeh/git-aware-prompt
+# https://github.com/jonmosco/kube-ps1
+# https://github.com/junegunn/fzf
+# https://github.com/cykerway/complete-alias
